1afc651987ac15305b210a7e48bc62faec1689e2
iflei01 <61976075+iflei01@users.noreply.github.com>
2023-11-15 18:33:37 +0100

    MERN Stack erstellt, Ordnerstruktur, close #3

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import connectDB from './config/db.js'
+import express from 'express'
+import dotenv  from 'dotenv'
+
+//connect database
+connectDB()
+
+//dotenv config
+dotenv.config()
+
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+//Express js listen method to run project on http://localhost:5000
+app.listen(PORT, console.log(`App is running in ${process.env.NODE_ENV} mode on port ${PORT}`))
