62c730759a71af2f443e2de6b4361a19a36bd5be
Simon Schröder <simon.p.s@t-online.de>
2023-11-16 10:43:04 +0100

    server the static files from the public folder to the express server and add a line to the html page

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,12 @@ dotenv.config()
 
 const app = express()
 
+// Serve static files from the 'public' directory
+app.use(express.static('../frontend/public'));
+
+
+
 const PORT = process.env.PORT || 3000
 
-//Express js listen method to run project on http://localhost:5000
+//Express js listen method to run project on http://localhost:3000
 app.listen(PORT, console.log(`App is running in ${process.env.NODE_ENV} mode on port ${PORT}`))
