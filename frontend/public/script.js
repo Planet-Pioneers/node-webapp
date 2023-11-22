eab43f7ac40148d0b04a35d1df64895ba0e710b5
sonja-becker <102386695+sonja-becker@users.noreply.github.com>
2023-11-22 14:28:20 +0100

    Frontend (#16)
    * Einbindung von Leaflet close #7
    
    * Einbindung Leaflat-Draw close #9
    
    * GeoJSON Uploadbereich und Textfeld close #8
    
    * Schieberegler und Button cloe #10 close #11
    
    * Download Buttons close #15
    
    ---------
    
    Co-authored-by: sbecker3 <sbecker3@uni-muenster.de>
    Co-authored-by: eglaser <eglaser@uni-muenster.de>

---
+++ frontend/public/script.js
@@ -0,0 +1,67 @@
+// Initialize Leaflet map
+const map = L.map('map').setView([51.96236, 7.62571], 15);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+// Only allow markers and polygons
+let drawControl = new L.Control.Draw({
+  draw: {
+    rectangle: true,
+    polygon: true,
+    circle: false,
+    circlemarker: false,
+    marker: false,
+    polyline: false,
+  }
+});
+map.addControl(drawControl);
+
+// Event listener for when a GeoJSON file is uploaded
+document.getElementById('geojson-file-input').addEventListener('change', function (e) {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+  
+    reader.onload = function (event) {
+      geojsonData = JSON.parse(event.target.result);
+    };
+  
+    reader.readAsText(file);
+  });
+
+// Event listener for the visualization button
+document.getElementById('resolution-slider').addEventListener('change', function () {
+    visualizeData();
+  });
+  
+  // Function to upload GeoJSON
+  function uploadGeoJSON() {
+    // You can add logic here to send the GeoJSON data to the server
+    console.log('GeoJSON uploaded:', geojsonData);
+  }
+  
+  // Function to visualize data on the map
+  function visualizeData() {
+    const resolution = document.getElementById('resolution-slider').value;
+    
+    // data on the map based on the selected resolution
+    console.log('Visualizing data with resolution:', resolution);
+  }
+  
+  // Function to save data
+  function saveData() {
+    const inputData = document.getElementById('data-input').value;
+    // save the data
+    console.log('Data saved:', inputData);
+  }
+
+  // Function to download training data
+function downloadTrainingData() {
+    // You can add logic here to initiate the download of training data
+    console.log('Downloading Training Data');
+  }
+  
+  // Function to download prediction
+  function downloadPrediction() {
+    // You can add logic here to initiate the download of prediction data
+    console.log('Downloading Prediction');
+  }
+  
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,21 +5,67 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Earth Classifier</h1>
+
+        {/* GeoJSON Upload Section */}
+        <div className="upload-section">
+          <h2>Upload GeoJSON</h2>
+          <p>Man kann entweder Dateien hochladen oder das Textfeld nutzen.</p>
+          <div>
+            <label>Dateien hochladen:</label>
+            <input type="file" id="geojson-file-input" />
+            <button className="btn btn-primary" onClick={() => uploadGeoJSON()}>Upload GeoJSON</button>
+          </div>
+          <div>
+            <label>Textfeld nutzen:</label>
+            <textarea id="data-input" rows="4" cols="50" placeholder="Enter data..."></textarea>
+            <button className="btn btn-primary" onClick={() => saveData()}>Save Data</button>
+          </div>
+
+        </div>
+        {/* Visualization Section */}
+        <div className="visualization-section">
+          <h2>Visualization</h2>
+          <input type="range" id="resolution-slider" min="1" max="10" value="5" />
+          <button className="btn btn-success" onClick={() => visualizeData()}>Visualize Data</button>
+        </div>
+
+        {/* Download Section */}
+        <div className="download-section">
+          <h2>Download</h2>
+          <button className="btn btn-info" onClick={() => downloadTrainingData()}>Download Training Data</button>
+          <button className="btn btn-info" onClick={() => downloadPrediction()}>Download Prediction</button>
+        </div>
+
       </header>
+      {/* Footer */}
+      <footer className="bg-light text-center text-lg-start mt-4">
+        <div className="text-center p-3" style={{ backgroundColor: '#f8f9fa' }}>
+          © 2023 Sonja, Simon, Inka, Elena, Arne
+        </div>
+      </footer>
     </div>
   );
 }
 
+// Placeholder functions
+function uploadGeoJSON() {
+  console.log('Upload GeoJSON logic');
+}
+
+function saveData() {
+  console.log('Save Data logic');
+}
+
+function visualizeData() {
+  console.log('Visualize Data logic');
+}
+
+function downloadTrainingData() {
+  console.log('Download Training Data logic');
+}
+
+function downloadPrediction() {
+  console.log('Download Prediction logic');
+}
 export default App;
