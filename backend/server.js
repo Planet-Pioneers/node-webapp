b3e6fe06cc6b75b1fb5bcc56fc162599583a2006
awilberg00 <103120839+awilberg00@users.noreply.github.com>
2023-12-18 14:39:15 +0100

    Revert "Node Verbindung für Dateiupload HTTP Request, style.css verschoben in…"

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,21 @@
-import connectDB from './config/db.js';
-import express from 'express';
-import bodyParser from 'body-parser';
-import dotenv from 'dotenv';
+import connectDB from './config/db.js'
+import express from 'express'
+import dotenv  from 'dotenv'
 
-// Connect database
-connectDB();
+//connect database
+connectDB()
 
-// Load environment variables
-dotenv.config();
+//dotenv config
+dotenv.config()
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+const app = express()
 
+// Serve static files from the 'public' directory
 app.use(express.static('../frontend/public'));
-app.use(bodyParser.json());
 
-// Route to receive GeoJSON data
-app.post('/uploadGeoJSON', (req, res) => {
-  const geoJSONData = req.body;
-  // Add logic here to work with the GeoJSON data
-  console.log('Received GeoJSON data:', geoJSONData);
-  res.status(200).json({ message: 'GeoJSON data received successfully' });
-});
 
-// Express.js listen method to run the project on http://localhost:3000
-app.listen(PORT, () => {
-  console.log(`App is running in ${process.env.NODE_ENV} mode on port ${PORT}`);
-});
+
+const PORT = process.env.PORT || 3000
+
+//Express js listen method to run project on http://localhost:3000
+app.listen(PORT, console.log(`App is running in ${process.env.NODE_ENV} mode on port ${PORT}`))
