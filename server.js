8d6d0c25e3db58acc6d20bfbd66aa8013666491c
Simon Schröder <simon.p.s@t-online.de>
2024-02-13 11:19:17 +0100

    add models endpoint to server.js and use older version of fetch to require it in commonjs

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const fetch = require('node-fetch');
 require("dotenv").config();
 
 const app = express();
@@ -38,6 +39,16 @@ app.get("/", (req, res) => {
     "Allow-access-Allow-Origin": '*'
   })
   return res.redirect('index.html');
+});
+app.get('/models', async (req, res) => {
+  try {
+    const response = await fetch('http://r-backend:8000/models');
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 
